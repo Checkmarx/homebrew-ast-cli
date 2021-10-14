4544280dbbce40e0c93683026f2d8b23ca292af8
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-14 11:43:21 +0100

    Brew formula update for ast-cli version 2.0.3

--- Formula/ast-cli.rb
+++ Formula/ast-cli.rb
@@ -5,29 +5,29 @@
 class AstCli < Formula
   desc "CLI for AST"
   homepage "https://github.com/Checkmarx/ast-cli"
-  version "2.0.2"
+  version "2.0.3"
   license "Apache"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.2/ast-cli_2.0.2_darwin_x64.tar.gz"
-      sha256 "977e9f468572495879a8ce7bda5c0412401fb02b13bb77c273f3a741d924accf"
+      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.3/ast-cli_2.0.3_darwin_x64.tar.gz"
+      sha256 "8d0f2b532cf47a5e903291c4eedeb23e980d727ab3269fd5b5ccb4c6c0627931"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.2/ast-cli_2.0.2_darwin_arm64.tar.gz"
-      sha256 "80828217d714ccf1bbcdfb1a6faa751c8d2091b2bdb07ddfe7e1f1378adf5305"
+      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.3/ast-cli_2.0.3_darwin_arm64.tar.gz"
+      sha256 "9e47ba97a10da5069187f6e29846c3f3413cfb3b6c4385702fae6f9008047099"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.2/ast-cli_2.0.2_linux_x64.tar.gz"
-      sha256 "98bd814e31216988d0e067e3d05ff3eb3ff3188452c628eccd0976cab1583ad3"
+      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.3/ast-cli_2.0.3_linux_x64.tar.gz"
+      sha256 "720383fc9628794c6e07151068d5e4ed83516a6875ac8be36201f1b36508c8b4"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.2/ast-cli_2.0.2_linux_arm64.tar.gz"
-      sha256 "1f71877c5da83de786c1d8d14269f95cd9d97b2b91e0c2490272f998ce978735"
+      url "https://github.com/Checkmarx/ast-cli/releases/download/2.0.3/ast-cli_2.0.3_linux_arm64.tar.gz"
+      sha256 "cf44cd223f971bb55db14690867e02b715dbb67839db11956f5f750c7931b174"
     end
   end
 
